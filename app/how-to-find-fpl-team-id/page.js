01f0f96c6bc1f843b92a19315c0be7dfb22e3bfb
Frank Omondi <frankomondi311@gmail.com>
2024-01-17 19:43:44 +0300

    UI updates

--- app/how-to-find-fpl-team-id/page.js
+++ app/how-to-find-fpl-team-id/page.js
@@ -24,7 +24,9 @@ export default function page() {
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className={`${lexend.className} mb-4 text-4xl tracking-tight font-bold text-[#06000c] dark:text-white`}>
+            <h2
+              className={`${lexend.className} mb-4 text-4xl tracking-tight font-bold text-[#06000c] dark:text-white`}
+            >
               How to find your FPL Team ID
             </h2>
             <p className="mb-4 font-light text-[#06000c]">
@@ -33,14 +35,13 @@ export default function page() {
             <div className="mb-4 text-[#06000c]">
               <ul className="list-decimal">
                 <li>
-                  Login to your FPL account and navigate to the points
-                  page
+                  Log in to your FPL account and navigate to the points page
                 </li>
                 <li>
                   The URL will have a unique number between /entry/ and /event/
                   which is your FPL team ID
                 </li>
-                <li>In the below example, the FPL Team ID is 7870392</li>
+                <li>In the example below, the FPL Team ID is 7870392</li>
               </ul>
               <p>
                 *You need to do this via a browser on a
